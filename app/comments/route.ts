8b157289809269188b689febf3e33bc6de50fca3
Joenll <manalastasjonelle@gmail.com>
2025-02-24 13:58:15 +1100

    patch and delete request

--- app/comments/route.ts
+++ app/comments/route.ts
@@ -1,7 +1,11 @@
 import {comments} from "./data";
+import { type NextRequest } from "next/server";
 
-export async function GET(){
-    return Response.json(comments);
+export async function GET(request: NextRequest){
+    const searchParams = request.nextUrl.searchParams;
+    const query = searchParams.get("query");
+    const filteredComment = query ? comments.filter((comment) => comment.text.includes(query)): comments;
+    return Response.json(filteredComment);
 }
 
 export async function POST(request: Request){
@@ -14,3 +18,4 @@ export async function POST(request: Request){
     comments.push(newComment);
     return Response.json(newComment)
 }
+
